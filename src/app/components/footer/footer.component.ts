8d377f971212e2b1b8081b0b1706fbb2baf4b734
Rojaon <bluerojs@gmail.com>
2023-11-23 05:31:10 +0300

    last modification

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  showFooter: boolean = true;
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+  
+  ngOnInit() {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.showFooter = !this.activatedRoute.firstChild?.snapshot.data['hideFooter'];
+      });
+  }
 
 }
